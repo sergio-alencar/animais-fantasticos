828cebc1f5471cb533a3047ecdeab183cd4082c8
Sérgio Alencar <sergiofalencar@gmail.com>
2024-04-18 13:12:13 -0300

    update

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -2,19 +2,19 @@
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import outsideClick from './outsideclick.js';
+import outsideClick from "./outsideClick.js";
 
 export default function initMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button"]');
   const menuList = document.querySelector('[data-menu="list"]');
-  const eventos = ['click', 'touchstart'];
+  const eventos = ["click", "touchstart"];
 
   function openMenu() {
-    menuList.classList.add('active');
-    menuButton.classList.add('active');
+    menuList.classList.add("active");
+    menuButton.classList.add("active");
     outsideClick(menuList, eventos, () => {
-      menuList.classList.remove('active');
-      menuButton.classList.remove('active');
+      menuList.classList.remove("active");
+      menuButton.classList.remove("active");
     });
   }
 
